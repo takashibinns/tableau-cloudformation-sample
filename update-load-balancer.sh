52ce96698130666bdaf7b055c2e41a135730ef74
Takashi Binns <takashi.binns@icloud.com>
2020-01-28 17:14:07 -0500

    fixed LB script

--- update-load-balancer.sh
+++ update-load-balancer.sh
@@ -6,12 +6,12 @@
 #######################################################################
 
 # Initialize variables
-TARGET_GROUP=$1; # ARN of the load balancer's target group
-REGION=$2; # Name of the AWS Region
+TARGET_GROUP=$1;   # ARN of the load balancer's target group
+REGION=$2;         # Name of the AWS Region
 STOP_INSTANCES=$3; # should be set to "true", if you want to stop the instance
 
 # Find existing instances
-IFS=' ' read -ra INSTANCES <<< $(aws elbv2 describe-target-health --target-group-arn $TARGET_GROUP --region $REGION --query "TargetHealthDescriptions[?TargetHealth.State=='healthy'].Target.Id" | $
+IFS=' ' read -ra INSTANCES <<< $(aws elbv2 describe-target-health --target-group-arn $TARGET_GROUP --region $REGION --query "TargetHealthDescriptions[?TargetHealth.State=='healthy'].Target.Id" | tr -dt '[],\"')
 
 # Loop through each instance
 for INSTANCE in ${INSTANCES[@]}
@@ -24,3 +24,9 @@ do
     aws ec2 stop-instances --region $REGION --instance-ids $INSTANCE;
   fi
 done
+
+# Find this instance's id
+NEW_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
+# Add this new instance to the load balancer
+aws elbv2 register-targets --target-group-arn $TARGET_GROUP --region $REGION --targets Id=$NEW_INSTANCE_ID
